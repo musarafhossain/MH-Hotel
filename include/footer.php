84aef529702ad0ec8dd90d4a91c02603d1777dfc
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-04-11 01:05:40 +0530

    separate header and footer

---
+++ include/footer.php
@@ -0,0 +1,71 @@
+<footer class="container-fluid bg-white mt-5">
+    <div class="row col-lg-12">
+        <div class="col-lg-3 p-4">
+            <h3 class="h-font fw-bold fs-3 mb-2">TJ HOTEL</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore suscipit itaque at vero consectetur?
+                Minus!</p>
+        </div>
+        <div class="col-lg-3 p-4">
+            <h5 class="mb-3">Links</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                Home
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                Rooms
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                Facilities
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                Contact Us
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                About
+            </a>
+        </div>
+        <div class="col-lg-3 p-4">
+            <h5 class="mb-3">Follow Us</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                <i class="bi bi-facebook"></i>
+                Facebook
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                <i class="bi bi-twitter"></i>
+                Twitter
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                <i class="bi bi-instagram"></i>
+                Instagram
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                <i class="bi bi-linkedin"></i>
+                Linkedin
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                <i class="bi bi-youtube"></i>
+                Youtube
+            </a><br>
+        </div>
+        <div class="col-lg-3 p-4">
+            <h5 class="mb-3">Others</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                Go to
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                Finder
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                Glocose
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                Category
+            </a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+                About
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by MH Hotel</h6>
+    </div>
+</footer>
