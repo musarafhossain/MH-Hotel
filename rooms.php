4ce34a139c418a136a07276bf49f97aedda15995
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-04-11 22:53:41 +0530

    add rooms page

---
+++ rooms.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MH Hotel - Rooms</title>
+    <!-- Common Links -->
+    <?php require_once('./include/links.php'); ?>
+</head>
+
+<body class="bg-light">
+    <!-- Navbar -->
+    <?php require_once('./include/header.php'); ?>
+
+    <!--Heading-->
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">OUR ROOMS</h2>
+        <div class="h-line bg-dark"></div>
+    </div>
+    
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+
+    <!--Footer-->
+    <?php require_once('./include/footer.php'); ?>
+
+    <!--Include Common Scripts-->
+    <?php require_once('./include/scripts.php'); ?>
+</body>
+
+</html>
