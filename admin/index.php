8ed818610b3d4bf2184663d736bbd12fec74f69b
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-04-12 21:07:43 +0530

    initialize admin section

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login Panel</title>
+    <!-- Common Links -->
+    <?php require_once('./include/links.php'); ?>
+</head>
+<body class="bg-light">
+    
+    <!--Include Common Scripts-->
+    <?php require_once('./include/scripts.php'); ?>
+</body>
+</html>
