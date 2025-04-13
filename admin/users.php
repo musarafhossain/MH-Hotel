c02c151b16752886931fde3d3fe5752d2f1903d7
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-04-13 18:36:59 +0530

    add admin rooms and users page

---
+++ admin/users.php
@@ -0,0 +1,30 @@
+<?php
+    require_once('./include/essentials.php');
+    adminLogin();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel - Users</title>
+
+    <!-- Common Links -->
+    <?php require_once('./include/links.php'); ?>
+</head>
+<body class="bg-white">
+    <!--Include Header-->
+    <?php require_once('./include/header.php'); ?>
+
+    <div class="container-fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+                <h1>Users Page</h1>
+            </div>
+        </div>
+    </div>
+
+    <!--Include Common Scripts-->
+    <?php require_once('./include/scripts.php'); ?>
+</body>
+</html>
