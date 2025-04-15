36f19fd9b133d714accffce2d6a41911c4010510
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-04-16 02:15:33 +0530

    make sub heading dynamic in facilities page

--- facilities.php
+++ facilities.php
@@ -24,7 +24,9 @@
     <div class="my-5 px-4">
         <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
         <div class="h-line bg-dark"></div>
-        <p class="text-center mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>Deleniti voluptatum totam et eum rem consectetur? Hic labore ipsum alias odio?</p>
+        <<p class="text-center mt-3 mx-auto" style="max-width: 700px;">
+            <?php echo $settings_r['site_about'] ?>
+        </p>
     </div>
     
     <!--Facilities Section-->
