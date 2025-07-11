5befa68d600e3a8b09607c9938b5658a1f107155
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-07-12 00:00:37 +0530

    fix redirect path for admin login to point to the correct dashboard

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
 
     session_start();
     if((isset($_SESSION['adminLogin']) && $_SESSION['adminLogin'] == true)){
-        redirect('dashboard.php');
+        redirect('admin/dashboard.php');
     }
 ?>
 
